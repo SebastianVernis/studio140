a6e25b86bb6fd93dabec806573a2a99b9fd8e94d
Chispa Delic <muertotv3@gmail.com>
2025-06-10 11:43:28 +0000

    No hay alguna fuente estilo Cyberpunk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Nunito', 'sans-serif'],
-        headline: ['Nunito', 'sans-serif'],
+        body: ['Orbitron', 'sans-serif'],
+        headline: ['Orbitron', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
